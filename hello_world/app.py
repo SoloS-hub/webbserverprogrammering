aff5de4064f0cebc1c203e031518b1c8cae96004
SoloS-hub <edvinlundh07@gmail.com>
2025-09-02 12:35:19 +0200

    .

--- hello_world/app.py
+++ hello_world/app.py
@@ -11,7 +11,6 @@ def hello_world():
 def show_user_profile(username):
     return f"User: {username}"
 
-
 @app.route('/search')
 def search():
     query = request.args.get('q', '')
@@ -24,15 +23,17 @@ def add():
     return f"a + b = {a + b}"
 
 @app.route('/login')
-def loogin():
-    return """<form action="/submit" method="post">
-            <label for="name">name:</label>
-            <input type="text" id="name" name="name"><br>
-            <label for="pasword">pasword:</label>
-            <input type="password" id="pasword" name="pasword"><br>
-            <input type="checkbox" id="checkbox" name="checkbox"><label for="checkbox">do you accept cookies</label> <br>
-            <input type="submit" value="Submit">
-        </form>"""
+def login():
+    return """
+<form action="/submit" method="post">
+    <label for="name">name:</label>
+    <input type="text" id="name" name="name"><br>
+    <label for="pasword">pasword:</label>
+    <input type="password" id="pasword" name="pasword"><br>
+    <input type="checkbox" id="checkbox" name="checkbox"><label for="checkbox">do you accept cookies</label> <br>
+    <input type="submit" value="Submit">
+</form>
+"""
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -40,6 +41,20 @@ def submit():
     pasword = request.form.get('pasword', '')
     checkbox = request.form.get('checkbox', False)
     return f'Received via POST: {name}, {pasword},  Checkbox: {checkbox}'
+
+@app.route('/templates/<username>')
+def templates(username):
+    return render_template("index.html", name=username)
+
+TOOLS_INFO = [
+    {"name": "hammer", "price": 9.99, "brand": "Acme", "stock": 12, "category": "hand tool"},
+    {"name": "drill", "price": 49.99, "brand": "Bosch", "stock": 5, "category": "power tool", "battery": "18V"},
+    {"name": "screwdriver_set", "price": 14.50, "brand": "Stanley", "stock": 20, "pieces": 10}
+]
+
+@app.route('/bob')
+def bob():
+    return render_template("bob.html", tools=TOOLS_INFO)
 
 if __name__ == '__main__':
     app.run(debug=True)

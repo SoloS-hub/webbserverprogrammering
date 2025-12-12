d1461ffa266e80ad7b3df355347dddfccd62856d
SoloS-hub <edvinlundh07@gmail.com>
2025-12-12 14:57:25 +0100

    .

--- hello_world/app.py
+++ hello_world/app.py
@@ -137,3 +137,4 @@ def handle_exception(error):
 
 if __name__ == '__main__':
     app.run(debug=True)
+ 
--- sockets/app.py
+++ sockets/app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
+import logging
 
-chatlog = []
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """To setup as many loggers as you want"""
+
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+chat_logger = setup_logger('chat_logger', 'chat.log')
+chat_logger.info('This is just info message')
 
 users = {}
 

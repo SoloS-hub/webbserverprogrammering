23fca2862456d1e1d612d470b86e5b1fc597667a
SoloS-hub <edvinlundh07@gmail.com>
2026-01-15 12:38:58 +0100

    .

--- sockets/app.py
+++ sockets/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import logging
+import os
+
+#find directory path for sockets
+filepath = os.path.dirname(os.path.abspath(__file__))
+
+if not os.path.exists(f"{filepath}/logs"):
+    os.makedirs(f"{filepath}/logs")
 
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
@@ -16,8 +23,7 @@ def setup_logger(name, log_file, level=logging.INFO):
 
     return logger
 
-chat_logger = setup_logger('chat_logger', 'chat.log')
-chat_logger.info('This is just info message')
+chat_logger = setup_logger('chat_logger', f"{filepath}/logs/chat.log")
 
 users = {}
 
@@ -36,7 +42,7 @@ def handle_join(username):
 
 @socketio.on("message")
 def message(message):
-    print(f"User: {users[request.sid]} has sent a message : {message}")
+    chat_logger.info(f"User: {users[request.sid]} has sent a message : {message}")
     emit("message", f"{users[request.sid]} sent: {message}", broadcast=True)
 
 
